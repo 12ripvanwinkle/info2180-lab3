4b97dd03a1db878f34506199848207c10ed01169
Nitesh <niteshanthony3@gmail.com>
2024-10-26 11:24:50 -0500

    Completed Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const boardSquares = document.querySelectorAll("#board div");
+
+    boardSquares.forEach (square => 
+    {
+        square.classList.add("square");
+    })
+})
